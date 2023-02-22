5f11bd95c22f8f4a5239f6a7aadaa5b5e46e9c7c
Ankur Rawat <ankurrawat620@gmail.com>
2023-02-23 02:55:38 +0530

    Complete Sidebar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React from 'react';
-import './App.css';
-import Header from './components/Header';
+import React from "react";
+import "./App.css";
+import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return (
-    <div className="app">
-        {/* TODO: Header */}
-        <Header/>
-        {/* TODO: App Body */}
-        {/* TODO: Sidebar */}
-        {/* TODO: Feed*/}
-        {/* TODO: Widgets */}
-    </div>
-  );
+    return (
+        <div className="app">
+            {/* TODO: Header */}
+            <Header />
+            {/* TODO: App Body */}
+            <div className="app__body">
+                {/* TODO: Sidebar */}
+                <Sidebar />
+                {/* TODO: Feed*/}
+                {/* TODO: Widgets */}
+            </div>
+        </div>
+    );
 }
 
 export default App;
+// ! 1:12:41 Video duration 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { Avatar } from "@mui/material";
+
+import "./Sidebar.css";
+
+function Sidebar() {
+    const recentItem = (topic) => (
+        <div className="sidebar__recentItem">
+            <span className="sidebar__hash">#</span>
+            <p>{topic}</p>
+        </div>
+    );
+
+    return (
+        <div className="sidebar">
+            <div className="sidebar__top">
+                <img
+                    src="https://images.unsplash.com/photo-1509023464722-18d996393ca8?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"
+                    alt=""
+                />
+                <Avatar className="sidebar__avatar" />
+                <h2>Ankur Rawat</h2>
+                <h4>ankurrawat620@gmail.com</h4>
+            </div>
+            <div className="sidebar__stats">
+                <div className="sidebar__stat">
+                    <p>Who viewed you</p>
+                    <p className="sidebar__statNumber">3.3M</p>
+                </div>
+                <div className="sidebar__stat">
+                    <p>Views on Post</p>
+                    <p className="sidebar__statNumber">12M</p>
+                </div>
+            </div>
+
+            <div className="sidebar__button">
+                <p>Recent</p>
+                {recentItem("reactJs")}
+                {recentItem("programming")}
+                {recentItem("softwareEngineering")}
+                {recentItem("design")}
+                {recentItem("developer")}
+            </div>
+        </div>
+    );
+}
+
+export default Sidebar;
